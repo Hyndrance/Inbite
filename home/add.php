7629dd619737e320bbe4a6d45de988c56f37b198
Dale Torre <Dale Torre>
2017-07-28 23:11:51 +0800

    Coded post function.

--- home/add.php
+++ home/add.php
@@ -1,56 +1,28 @@
 
-<form action="process.php?action=add" method="POST" enctype="multipart/form-data">
+<form >
 
-<label>DESCRIPTION *</label>
-<textarea name="description" placeholder="Input description here"></textarea></br>
-</br>
-<label>UPLOAD PHOTO *</label>
-	<input type="file" name="upload_file"/><br></br>
-	<input type="hidden" name="username" value="<?=$username;?>">
-	
-	
-	<input type="submit" value="POST">
 </form>
 
 <div class="row clearfix">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
-                        <div class="header">
-                            <h2>
-                                VERTICAL LAYOUT
-                            </h2>
-                            <ul class="header-dropdown m-r--5">
-                                <li class="dropdown">
-                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                        <i class="material-icons">more_vert</i>
-                                    </a>
-                                    <ul class="dropdown-menu pull-right">
-                                        <li><a href="javascript:void(0);">Action</a></li>
-                                        <li><a href="javascript:void(0);">Another action</a></li>
-                                        <li><a href="javascript:void(0);">Something else here</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
                         <div class="body">
-                            <form>
-                                <label for="email_address">Email Address</label>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="text" id="email_address" class="form-control" placeholder="Enter your email address">
-                                    </div>
-                                </div>
-                                <label for="password">Password</label>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="password" id="password" class="form-control" placeholder="Enter your password">
-                                    </div>
-                                </div>
-
-                                <input type="checkbox" id="remember_me" class="filled-in">
-                                <label for="remember_me">Remember Me</label>
-                                <br>
-                                <button type="button" class="btn btn-primary m-t-15 waves-effect">LOGIN</button>
+                            <form action="process.php?action=add" method="POST" enctype="multipart/form-data">
+								<h2 class="card-inside-title">Description</h2>
+								<div class="row clearfix">
+									<div class="col-sm-12">
+										<div class="form-group">
+											<div class="form-line">
+												<textarea rows="4" class="form-control no-resize" name="description" placeholder="Please type what you want..."></textarea>
+											</div>
+										</div>
+									</div>
+								</div>
+                                <label>Upload Photo</label>
+								<input type="file" name="upload_file"/><br></br>
+								<input type="hidden" name="username" value="<?=$username;?>">
+								
+								<button type="submit" class="btn btn-primary m-t-15 waves-effect">POST</button>
                             </form>
                         </div>
                     </div>
